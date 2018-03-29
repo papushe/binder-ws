30c9accca5785db585ee63822d70edef850eb474
NaorHai <Na1994or>
2018-03-29 14:20:41 +0300

    newManager selection

--- services/communityService.js
+++ services/communityService.js
@@ -1,21 +1,23 @@
 let COMMUNITY = require('../models/Community');
 
 exports.getNextNewManagerId = (community) => {
+    let newManagerId = null;
 
     if (community.authorizedMembers.length > 0) {
         community.authorizedMembers.forEach(authMember => {
             if (authMember.memberId != community.managerId) {
-                return authMember.memberId;
+                newManagerId = authMember.memberId;
             }
         })
     }
     if (community.members.length > 0) {
         community.members.forEach(member => {
             if (member.memberId != community.managerId) {
-                return member.memberId;
+                newManagerId = member.memberId;
             }
         })
     }
+    return newManagerId;
 };
 
 
