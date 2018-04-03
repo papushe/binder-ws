03cabe6e8a8c4161d09b30a38c52767fda0ec9a8
NaorHai <Na1994or>
2018-04-03 18:06:29 +0300

    robust connection to db, override default time out

--- database.js
+++ database.js
@@ -4,25 +4,31 @@ const consts   = require('./config').MLAB_KEY,
 
 mongoose.Promise = global.Promise;
 //The server option auto_reconnect is defaulted to true
-var options = {
+let options = {
     server: {
         auto_reconnect:true,
-        useMongoClient: false
+        useMongoClient: false,
+        keepAlive: 300000,
+        connectTimeoutMS: 60000
     }
 };
-mongoose.connect(consts, options);
-const conn = mongoose.connection;//get default connection
-// Event handlers for Mongoose
-conn.on('error', function (err) {
-    console.log('Mongoose: Error: ' + err);
-});
-conn.on('open', function() {
-    console.log('Mongoose: Connection established');
-});
-conn.on('disconnected', function() {
-    console.log('Mongoose: Connection stopped, recconect');
-    mongoose.connect(consts, options);
-});
-conn.on('reconnected', function () {
-    console.info('Mongoose reconnected!');
+
+mongoose.connect(consts, options).then(() => {
+    const conn = mongoose.connection;//get default connection
+    // Event handlers for Mongoose
+    conn.on('error', (err) => {
+        console.log('Mongoose: Error: ' + err);
+    });
+    conn.on('open', () => {
+        console.log('Mongoose: Connection established');
+    });
+    conn.on('disconnected', () => {
+        console.log('Mongoose: Connection stopped, recconect');
+        mongoose.connect(consts, options);
+    });
+    conn.on('reconnected', () => {
+        console.info('Mongoose reconnected!');
+    });
+}, (err) => {
+    console.log(`Failed to connect DB  - reason:  ${err}`);
 });
